81c50e8affb3eb0735fd4c94b658c753e154a2da
theShaurman <barbi.20639@gmail.com>
2025-12-23 02:56:37 +0300

    Add files via upload

---
+++ project-title/app-files/data.js
@@ -0,0 +1,251 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "УЛИЦА",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 384,
+      "initialViewParameters": {
+        "yaw": -1.9276672301702984,
+        "pitch": -0.1422788714999541,
+        "fov": 1.4069669810135341
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.20358457327967,
+          "pitch": 0.47779024856164654,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": 0.9819415517817482,
+          "pitch": 0.2884142592022467,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-",
+      "name": "БАНЯ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 384,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8437550857755047,
+          "pitch": 0.518892872042402,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": -1.110269271369864,
+          "pitch": 0.4257697871709425,
+          "rotation": 5.497787143782138,
+          "target": "5-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-",
+      "name": "ЛОББИ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 384,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5108151303326274,
+          "pitch": 0.5906685204821187,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": -1.2065055342777953,
+          "pitch": 0.5216051741884069,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-",
+      "name": "БАР",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 384,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.634580764714446,
+          "pitch": 0.18270710249620592,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": 1.5412196835349992,
+          "pitch": 0.12794305537702755,
+          "rotation": 4.71238898038469,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-",
+      "name": "БИЛЬЯРД",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 384,
+      "initialViewParameters": {
+        "yaw": 1.982706467445782,
+        "pitch": -0.08260962119459947,
+        "fov": 1.4069669810135341
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.27851803681486587,
+          "pitch": 0.002137568451249905,
+          "rotation": 7.0685834705770345,
+          "target": "3-"
+        },
+        {
+          "yaw": 1.4322199040159074,
+          "pitch": 0.08321607883973137,
+          "rotation": 0.7853981633974483,
+          "target": "6--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-",
+      "name": "БАССЕЙН",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 384,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8032590847312484,
+          "pitch": 0.09621571734530932,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6--",
+      "name": "НОМЕР ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 384,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9459621942634033,
+          "pitch": 0.3392300691781962,
+          "rotation": 1.5707963267948966,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
